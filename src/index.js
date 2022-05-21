c6a9888933b5de5c13adffa7c0161957e36a2be6
Aman Choudhary <63499156+amanchoudhary18@users.noreply.github.com>
2022-05-21 18:30:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,18 +6,6 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT;
 
-// const multer = require("multer");
-// const upload = multer({
-//   dest: "images",
-//   limits: {
-//     fileSize: 1000000,
-//   },
-// });
-
-// app.post("/upload", upload.single("upload"), (req, res) => {
-//   res.send();
-// });
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
